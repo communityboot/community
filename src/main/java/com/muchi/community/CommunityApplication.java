be611c288773f55a1e7ece2557e68ea0d410ce05
472972984@qq.com <472972984@qq.com>
2019-08-14 00:03:55 +0800

    兼容 tomcat 启动项目

--- src/main/java/com/muchi/community/CommunityApplication.java
+++ src/main/java/com/muchi/community/CommunityApplication.java
@@ -2,9 +2,16 @@ package com.muchi.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CommunityApplication {
+public class CommunityApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CommunityApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
