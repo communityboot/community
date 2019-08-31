d9722bebd17043dda3933efcd2461f4cda2fca1d
yuzq <1065265077@qq.com>
2019-08-31 22:44:07 +0800

    添加返回值静态方法

--- src/main/java/com/muchi/community/common/utils/LayuiVo.java
+++ src/main/java/com/muchi/community/common/utils/LayuiVo.java
@@ -6,11 +6,11 @@ package com.muchi.community.common.utils;
  * @Date: 2019/7/29   22:43
  */
 public class LayuiVo {
-    protected Integer code;
+    private Integer code;
 
     protected String msg;
 
-    protected Long count;
+    private Long count;
 
     protected Object data;
 
@@ -24,12 +24,17 @@ public class LayuiVo {
 
     public LayuiVo(){}
 
-    public LayuiVo(Integer code, String msg, Long total, Object data) {
+    private LayuiVo(Integer code, String msg, Long total, Object data) {
         super();
         this.code = code;
         this.msg = msg;
-        this.count = count;
+        this.count = total;
         this.data = data;
+    }
+
+    public LayuiVo(Integer code,String msg){
+        this.code=code;
+        this.msg=msg;
     }
 
     public Integer getCode() {
@@ -54,5 +59,72 @@ public class LayuiVo {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+
+    /**
+     * 请求成功并返回数据
+     * @param total 总数
+     * @param data 数据
+     * @return 数据
+     */
+    public static LayuiVo successByData(Long total,Object data){
+        return new LayuiVo(200,"请求成功！",total,data);
+    }
+
+    /**
+     *
+     * @return 请求成功提示信息
+     */
+    public static LayuiVo successByMsg(){
+        return new LayuiVo(200,"请求成功！");
+    }
+
+    /**
+     * @return 请求失败提示信息
+     */
+    public static LayuiVo failByMsg(){
+        return new LayuiVo(200,"请求失败！");
+    }
+
+    /**
+     * @return 错误信息带数据
+     */
+    public static LayuiVo failByData(Long total,Object data){
+        return new LayuiVo(100,"请求失败！",total,data);
+    }
+
+    /**
+     * @return 参数错误信息返回信息
+     */
+    public static LayuiVo paramError( ){
+        return new LayuiVo(400,"参数错误！");
+    }
+
+
+    /**
+     * 需要登录
+     * @return 返回需要登录的Json信息
+     */
+    public static LayuiVo needLogin(){
+        return new LayuiVo(401,"请登录!");
+    }
+
+    /**
+     * 需要登录
+     * @return 返回需要登录的Json信息
+     */
+    public static LayuiVo needAuth(){
+        return new LayuiVo(403,"当前用户无权限!");
+    }
+
+    @Override
+    public String toString() {
+        return "LayuiVo{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", count=" + count +
+                ", data=" + data +
+                '}';
     }
 }
