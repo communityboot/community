8961b5988a8bdff55f311c97d9e473829a0ef436
yuzq <1065265077@qq.com>
2019-09-23 17:29:58 +0800

    1.添加js工具类 2.

---
+++ src/main/resources/static/js/BaseUtil.js
@@ -0,0 +1,21 @@
+/**
+ * 定义请求前缀
+ * @type {string}
+ */
+let baseUtil='http://localhost:8088';
+
+/**
+ * @param url 请求地址
+ * @param data 传给后端的数据
+ * @param callback 自定义成功的方法
+ */
+function ajaxDemo(url,data,callback) {
+    $.ajax({
+        type:'post',
+        url:url,
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        data:data,
+        success:callback,
+    })
+}
