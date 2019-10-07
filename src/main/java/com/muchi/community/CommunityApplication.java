421eef31f9e5b36fc747f3a6ff40e1491971918a
yuzq <1065265077@qq.com>
2019-10-07 09:39:48 +0800

    1.新增fastDFS客户端工具类 2.启动类添加fastDFS配置 3.更改用户页面布局，简化代码 4.新增图片上传测试类 5.添加文件上传下载依赖

--- src/main/java/com/muchi/community/CommunityApplication.java
+++ src/main/java/com/muchi/community/CommunityApplication.java
@@ -1,11 +1,17 @@
 package com.muchi.community;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class CommunityApplication extends SpringBootServletInitializer {
 
     @Override
---
+++ src/test/java/com/muchi/community/FastDfsTest.java
@@ -0,0 +1,33 @@
+package com.muchi.community;
+
+import com.muchi.community.common.utils.FastDFSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
+/**
+ * @Author： yuzq
+ * @Description:
+ * @Date: 2019/10/7   9:24
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class FastDfsTest {
+
+    @Test
+    public void testUpload(){
+        String fileUrl=this.getClass().getResource("/IMG_105.JPG").getPath();
+        System.out.println(fileUrl);
+        File file=new File(fileUrl);
+        String string= FastDFSClient.uploadFile(file);
+        FastDFSClient.getResAccessUrl(string);
+    }
+
+    @Test
+    public void testDelete(){
+        FastDFSClient.deleteFile("group1/M00/00/00/wKgBvF2alTSAC5gcAAEcz00quPA995.JPG");
+    }
+}
